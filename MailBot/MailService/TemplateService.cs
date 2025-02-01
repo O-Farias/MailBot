86f006e548ecabf52c54a1e48f2e75b68f1994e7
Mateus Farias <mateus.f2001@hotmail.com>
2025-02-01 16:06:07 +0000

    feat: implementa serviço de templates para emails

---
+++ MailBot/MailService/TemplateService.cs
@@ -0,0 +1,41 @@
+using MailBot.Models;
+
+namespace MailBot.MailService;
+
+public class TemplateService
+{
+    private readonly Dictionary<string, EmailTemplate> _templates = new();
+
+    public void AddTemplate(string name, EmailTemplate template)
+    {
+        _templates[name] = template;
+    }
+
+    public EmailTemplate GetTemplate(string name)
+    {
+        if (!_templates.ContainsKey(name))
+            throw new KeyNotFoundException($"Template '{name}' não encontrado.");
+
+        return _templates[name];
+    }
+
+    public EmailMessage ProcessTemplate(string templateName, Dictionary<string, string> variables)
+    {
+        var template = GetTemplate(templateName);
+        var subject = template.Subject;
+        var body = template.Body;
+
+        foreach (var variable in variables)
+        {
+            subject = subject.Replace($"{{{variable.Key}}}", variable.Value);
+            body = body.Replace($"{{{variable.Key}}}", variable.Value);
+        }
+
+        return new EmailMessage
+        {
+            Subject = subject,
+            Body = body,
+            IsHtml = template.IsHtml
+        };
+    }
+}
